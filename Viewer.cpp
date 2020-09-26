745d3c8986f7ab195a216764efd718b9042bf204
Stephen Stammen <robotcorner88@gmail.com>
2020-09-26 01:10:25 -0500

    created the header and definition file for the viewer class

---
+++ Viewer.cpp
@@ -0,0 +1,104 @@
+#include "Viewer.h"
+#include <iostream>
+
+Viewer::Viewer()
+{
+	this->header_message = "DOCO World Simulation";
+	this->footer_message = "- Stephen Stammen";
+	this->separator_line = std::string(this->separator_length, this->separator_char);
+	this->program_state_message = "paused";
+}
+
+Viewer::~Viewer()
+{
+
+}
+
+void Viewer::setHeaderMessage(std::string newHeader)
+{
+	if (newHeader.length() < 100) {
+		this->header_message;
+	}
+	else
+	{
+		this->header_message = "Too Long Header Message Attempted";
+	}
+}
+
+void Viewer::setFooterMessage(std::string newFooter)
+{
+	if (newFooter.length() < 100) {
+		this->footer_message;
+	}
+	else
+	{
+		this->footer_message = "Too Long Footer Message Attempted";
+	}
+}
+
+void Viewer::setProgramStateMessage(std::string newState)
+{
+	this->program_state_message;
+}
+
+void Viewer::setLineCountRange(int lineCount)
+{
+	this->line_count_world = lineCount;
+}
+
+void Viewer::setSeparatorLength(int newLength)
+{
+	if (newLength < 100) // prevent more than 150 characters for separation
+	{
+		this->separator_length = newLength;
+	}
+	else
+	{
+		this->separator_length = 100;
+	}
+}
+
+void Viewer::setNewSeparatorLine(void)
+{
+	this->separator_line = std::string(this->separator_length, this->separator_char);
+}
+
+std::string Viewer::getHeaderMessage(void)
+{
+	return this->header_message;
+}
+
+std::string Viewer::getFooterMessage(void)
+{
+	return this->footer_message;
+}
+
+std::string Viewer::getProgramStateMessage(void)
+{
+	return this->program_state_message;
+}
+
+std::string Viewer::getNewSeparatorLine(void)
+{
+	return this->separator_line;
+}
+
+void Viewer::printSeparator(void)
+{
+	std::cout << this->separator_line << "\n";
+}
+
+int Viewer::getLineCountWorld(void) 
+{
+	return this->line_count_world;
+}
+
+int Viewer::getSeparatorLength(void) 
+{
+	return this->separator_length;
+}
+
+char Viewer::getSeparatorChar(void)
+{
+	return this->separator_char;
+}
---
+++ Viewer.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+
+// the viewer class is just to hold some data for what will be output to the console.
+
+class Viewer
+{
+private:
+	std::string header_message; // holds the start message at the top of the console.
+	int line_count_world; // keeps the total line count, basically number of matrix rows.
+	int separator_length = 75;
+	char separator_char = '.';
+	std::string separator_line;
+	std::string footer_message; // text at bottom of console.
+	std::string program_state_message; // paused / play state
+
+public:
+	Viewer(); // create a viewer object
+	~Viewer(); // destroy the the viewer object
+	
+	void setHeaderMessage(std::string); // update header_message
+	void setFooterMessage(std::string); // update footer_message
+	void setProgramStateMessage(std::string);	// update paused / play message
+	void setLineCountRange(int); // update line_count_range
+	void setSeparatorLength(int); // set length of the separator
+	void setSeparatorChar(char); // set the separator character
+	void setNewSeparatorLine(void); // create new separator based off new settings
+
+	std::string getHeaderMessage(void); // return header message
+	std::string getFooterMessage(void);	// return footer message
+	std::string getProgramStateMessage(void);	// get paused / play mesage
+	std::string getNewSeparatorLine(void); // get separator string
+	void printSeparator(void); // print separator string to console with new line
+	int getLineCountWorld(void); // get line count
+	int getSeparatorLength(void); // get separator length
+	char getSeparatorChar(void); // get the char used in the separator
+};
+
